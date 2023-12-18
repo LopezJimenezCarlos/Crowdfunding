688662d83b16982a384b5e399e75818ab16495fb
LopezJimenezCarlos <lopezcarlosuem@gmail.com>
2023-12-18 11:00:55 +0100

    Cambios_varios

--- js/script.js
+++ js/script.js
@@ -7,11 +7,13 @@ function scrollFunction() {
         logo.style.width = "60px"; // Tamaño más pequeño al hacer scroll        
         logo.style.height = "50px"; // Tamaño más pequeño al hacer scroll
         logo.style.transition = "0.6s"; // Transición de 0.5 segundos
-        
+        logo.style.position = "relative"; // Transición de 0.5 segundos
+        barraSuperior.style.boxShadow=" 0px 5px 3px rgba(0, 0, 0, 0.2)";
     } else {
         barraSuperior.style.height = "100px"; // Altura normal en la parte superior
         logo.style.width = "100px"; // Tamaño más pequeño al hacer scroll        
         logo.style.height = "95px"; // Tamaño más pequeño al hacer scroll
+        barraSuperior.style.boxShadow= " 0px 10px 3px rgba(0, 0, 0, 0.2)";
     }   
 }
 window.onscroll = function() {
@@ -22,4 +24,14 @@ var filas = respuestaDelServidor;
 
 var nombre = document.getElementById("Usuario");
 nombre.innerText = filas[0].nombre;
+
+function toggleBusqueda() {
+    const campoBusqueda = document.getElementById('campoBusqueda');
+    const linkBuscar = document.getElementById('linkBuscar');
+
+    campoBusqueda.classList.toggle('mostrar');
+    campoBusqueda.style.display = campoBusqueda.classList.contains('mostrar') ? 'block' : 'none';
+    campoBusqueda.focus();
+    linkBuscar.style.display = campoBusqueda.classList.contains('mostrar') ? 'none' : 'inline';
+}
 
