379aabb864557457e26424fd75624f29062114d4
LopezJimenezCarlos <lopezcarlosuem@gmail.com>
2023-12-21 11:05:03 +0100

    Donaciones

--- js/server.js
+++ js/server.js
@@ -10,56 +10,81 @@ const server = http.createServer((req, res) => {
     if (filePath == './') {
         filePath = './index.html';
 
-        // Realiza la consulta a la base de datos
-        realizarConsulta(res);
+        // Realiza ambas consultas a la base de datos
+        realizarConsultas(res);
     } else {
         // Maneja la solicitud para script.js y otros archivos estáticos
         serveStaticFile(res, filePath);
     }
 });
 
-function realizarConsulta(res) {
+function realizarConsultas(res) {
     let conexion = mysql.createConnection({
         host: "localhost",
-        database: "test",
+        database: "stylehaven",
         user: "root",
         password: "",
     });
 
-    // Creamos la conexion y vemos si hay error o se conecta correctamente
+    // Creamos la conexión y vemos si hay error o se conecta correctamente
     conexion.connect(function (error) {
         if (error) {
             throw error;
         } else {
-            console.log("Conexion exitosa");
-        }
-    });
+            console.log("Conexión exitosa");
 
-    conexion.query("SELECT * FROM alumnado", function (error, filas) {
-        if (error) {
-            throw error;
-        } else {
-            // Lee el contenido de index.html
-            fs.readFile('./index.html', 'utf-8', (err, content) => {
-                if (err) {
-                    res.writeHead(500);
-                    return res.end('Error loading index.html');
+            // Consulta 1: Obtener la suma total de la columna "cantidad"
+            conexion.query("SELECT SUM(cantidad) AS totalDonado FROM donaciones", function (error, resultado1) {
+                if (error) {
+                    throw error;
+                } else {
+                    // Consulta 2: Obtener los donadores con las mayores donaciones
+                    conexion.query("SELECT Nombre, Apellidos, SUM(cantidad) AS totalDonado FROM donaciones GROUP BY Nombre, Apellidos ORDER BY totalDonado DESC LIMIT 8", function (error, filas) {
+                        if (error) {
+                            throw error;
+                        } else {
+                            // Lee el contenido de index.html
+                            fs.readFile('./index.html', 'utf-8', (err, content) => {
+                                if (err) {
+                                    res.writeHead(500);
+                                    return res.end('Error loading index.html');
+                                }
+
+                                // Procesa los resultados de ambas consultas
+                                const totalDonado = resultado1[0].totalDonado;
+                                const donadores = filas.map((fila) => ({
+                                    nombre: fila.Nombre,
+                                    apellidos: fila.Apellidos,
+                                    totalDonado: fila.totalDonado
+                                }));
+
+                                // Crea un contenedor con la clase "contenedor_donadores"
+                                const contenedorDonadoresHTML = '<div class="contenedor_donadores">' +
+                                    donadores.map((donador) => `
+                                        <div class="donador">
+                                            <p>${donador.nombre} ${donador.apellidos}</p>
+                                            <p>${donador.totalDonado} €</p>
+                                        </div>
+                                    `).join('') +
+                                    '</div>';
+
+                                // Inserta los resultados de las consultas en el contenido de index.html
+                                content = content.replace('{{DATA1}}', "Cantidad total donada: " + totalDonado + " €");
+                                content = content.replace('{{DATA2}}', contenedorDonadoresHTML);
+
+                                // Envía el contenido actualizado al cliente
+                                res.writeHead(200, { 'Content-Type': 'text/html' });
+                                res.end(content, 'utf-8');
+
+                                // Cierra la conexión a la base de datos después de completar las operaciones
+                                conexion.end();
+                            });
+                        }
+                    });
                 }
-
-                // Inserta los resultados de la consulta en el contenido de index.html
-                const nombres = filas.map((fila) => fila.Nombre + ' '+ fila.Apellidos).join(', ');
-               
-                content = content.replace('{{DATA}}', nombres);
-                
-                
-                // Envía el contenido actualizado al cliente
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(content, 'utf-8');
             });
         }
     });
-
-    conexion.end();
 }
 
 function serveStaticFile(res, filePath, contentType = 'application/octet-stream') {
